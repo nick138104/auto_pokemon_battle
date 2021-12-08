742047060b80ddccc1473468926cba12096d9127
Supakorn <nick.supakorn@hotmail.com>
2021-12-09 01:36:25 +0700

    update

---
+++ src/entity/Food.java
@@ -0,0 +1,12 @@
+package entity;
+
+import entity.base.Item;
+
+public class Food extends Item {
+
+	public Food(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/entity/base/Item.java
+++ src/entity/base/Item.java
@@ -1,6 +1,20 @@
 package entity.base;
 
-public abstract class Item {
-	
+public abstract class Item extends Gameobject {
+
 	private String name;
+
+	public Item(String name) {
+		super();
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
