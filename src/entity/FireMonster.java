74e4c3f6396af21233e0f04a04bddeac9040d7b1
Supakorn <nick.supakorn@hotmail.com>
2021-12-08 23:59:15 +0700

    First Update

---
+++ src/entity/FireMonster.java
@@ -0,0 +1,13 @@
+package entity;
+
+import entity.base.Monster;
+import entity.base.MonsterElement;
+
+public class FireMonster extends Monster {
+
+	public FireMonster(String name, int attack, int lifepoint, MonsterElement element) {
+		super(name, attack, lifepoint, element);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/entity/base/Item.java
@@ -0,0 +1,5 @@
+package entity.base;
+
+public abstract class Item {
+	
+}
